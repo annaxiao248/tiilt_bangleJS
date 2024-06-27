e821a6f0a3c6fd7371757604cecc6960bb48b392
annaxiao248 <100732427+annaxiao248@users.noreply.github.com>
2024-06-27 01:23:02 -0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,49 @@
-'use client'
+'use client';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 import UploadSubmitPage from './uploadSubmit';
 import FetchDisplayPage from './fetchDisplay';
 
 function Home() {
+  const location = useLocation();
+  
+  return (
+    <div>
+      <nav className="flex space-x-4 bg-slate-800 p-4 rounded-lg">
+        <ul className="flex space-x-4">
+          <li>
+            <Link 
+              to="/" 
+              className={`p-2 ${location.pathname === '/' ? 'text-white bg-blue-500 rounded' : 'text-blue-500'}`}
+            >
+              Upload & Submit
+            </Link>
+          </li>
+          <li>
+            <Link 
+              to="/fetch" 
+              className={`p-2 ${location.pathname === '/fetch' ? 'text-white bg-blue-500 rounded' : 'text-blue-500'}`}
+            >
+              Fetch Data
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<UploadSubmitPage />} />
+        <Route path="/fetch" element={<FetchDisplayPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Upload & Submit</Link>
-            </li>
-            <li>
-              <Link to="/fetch">Fetch Data</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<UploadSubmitPage />} />
-          <Route path="/fetch" element={<FetchDisplayPage />} />
-        </Routes>
-      </div>
+      <Home />
     </Router>
   );
 }
 
-export default Home;
+export default App;
